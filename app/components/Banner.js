257560c69c2d74dd8292f42bd6954f3bce02f1b3
IndriMaharani <indrimaharani446@gmail.com>
2025-01-09 22:16:00 +0700

    indri : membuat baner

---
+++ app/components/Banner.js
@@ -0,0 +1,9 @@
+export default function Banner() {
+    return (
+      <section style={{ textAlign: 'center', padding: '2rem', backgroundColor: '#FFD700' }}>
+        <h2>Selamat Datang di CitaRasaID</h2>
+        <p>Temukan oleh-oleh khas daerah dengan harga terbaik!</p>
+      </section>
+    );
+  }
+  
