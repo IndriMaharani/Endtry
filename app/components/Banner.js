78fa25d527d37f104daf66d2947e07b76578b1bd
lupii123 <@luffi_rahmawati@teknokrat.ac.id>
2025-01-16 18:13:33 +0700

    Meyka : Ubah banner

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -2,7 +2,7 @@ export default function Banner() {
     return (
       <section style={{ textAlign: 'center', padding: '2rem', backgroundColor: '#FFD700' }}>
         <h2>Selamat Datang di CitaRasaID</h2>
-        <p>Temukan oleh-oleh khas daerah dengan harga terbaik!</p>
+        <p>Jelajahi Kuliner Nusantara</p>
       </section>
     );
   }
